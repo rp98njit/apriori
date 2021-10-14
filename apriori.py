86d4afe37cefdce924bcf91aca4063d1c6eb06ba
Rajendra Prasad Patil <rp98@njit.edu>
2021-10-14 21:38:49 +0000

    kk

--- apriori.py
+++ apriori.py
@@ -1,12 +1,17 @@
 import pandas as pd
 df = pd.read_csv('sample_data.tsv', sep='\t')
 
+# item_obj stores information related to each item, such as count, support, transaction ID (as a set)
 item_obj = {}
+
 transaction_count = df.shape[0]
 min_support = 0.5
 min_confidence = 0.7
 
-for record in df['Transaction']:
+for index, row in df.iterrows():
+    record = row['Transaction']
+    tid = row['Transaction']
+    
     # splitting each item
     items = record.split(',')
     # removing trailing spaces, lines for each item and converting it to lower case
@@ -15,12 +20,14 @@ for record in df['Transaction']:
     items = list(set(items))
     for item in items:
         if item in item_obj:
-            item_obj[item]['count'] = item_obj[item]['count'] + 1
+            # updating the existing object in the dictionary
+            item_obj[item]['cnt'] = item_obj[item]['cnt'] + 1
+            item_obj[item]['tranx_id'].add(tid)
         else:
-            item_obj[item] = {'count': 1}
-
+            # creating an object in the dictionary for the first time
+            item_obj[item] = {'cnt': 1}
+            item_obj[item] = {'tranx_id': {tid}}
+    
+# calculating the support for each item
 for key in item_obj:
-    item_obj[key]['support'] = (item_obj[key]['count'])/transaction_count
-
-print('Hello')
-
+    item_obj[key]['support'] = (item_obj[key]['cnt'])/transaction_count
