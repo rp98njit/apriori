edb16c0174847c00bca074bf24edfe528ef40f23
rp98njit <rp98@njit.edu>
2021-10-21 11:03:37 -0400

    finding the association rules is done

--- apriori.py
+++ apriori.py
@@ -1,5 +1,6 @@
 import itertools
 from pprint import pprint
+
 import pandas as pd
 
 df = pd.read_csv('sample_data.tsv', sep='\t')
@@ -40,25 +41,24 @@ def init_one():
             else:
                 # creating an object in the dictionary for the first time
                 item_obj[item] = {'cnt': 1, 'transactions': {tid}}
-            # calculating the support
-            item_obj[item]['support'] = (item_obj[item]['cnt']) / transaction_count
-            # check minimum support
-            if item_obj[item]['support'] >= min_support:
-                if 1 in support_index and item not in support_index[1]:
-                    support_index[1].append(item)
-            else:
-                if 1 in support_index and item in support_index[1]:
-                    support_index[1].remove(item)
+
+    for item in list(item_obj):
+        support = (item_obj[item]['cnt']) / transaction_count
+        if support < min_support:
+            del item_obj[item]
+        else:
+            item_obj[item].update({'support': support})
+            support_index[1].append(item)
 
 
 def next_step():
     global current_list_level
     current_support_items = support_index[1]
-    # create combinations from previous level
-    item_combinations = list(itertools.combinations(support_index[1], 2))
+    association_rules = {}
 
     while len(current_support_items) > 0:
         current_list_level += 1
+        # create combinations from previous level
         item_combinations = list(itertools.combinations(current_support_items,
                                                         current_list_level))
         current_support_items = []
@@ -78,6 +78,39 @@ def next_step():
                 current_support_items = list(set(current_support_items))
                 item_obj.update({combination: {'cnt': 1, 'transactions': common_transactions,
                                                'support': current_support}})
+
+    for item in item_obj:
+        if type(item) == str:
+            continue
+
+        for i in range(1, 2**len(item)-1):
+            right_side = []
+            indexes = '{0:b}'.format(i)
+            indexes = '0'*(len(item)-len(indexes)) + indexes
+
+            for j, value in enumerate(indexes):
+                if value == '1':
+                    right_side.append(item[j])
+            left_side = [each_item for each_item in item if each_item not in right_side]
+            if len(left_side) == 1:
+                left_side = left_side[0]
+            else:
+                left_side = tuple(left_side)
+
+            if len(right_side) == 1:
+                right_side = right_side[0]
+            else:
+                right_side = tuple(right_side)
+
+            confidence = item_obj[item]['support'] / item_obj[left_side]['support']
+            if confidence < min_confidence:
+                continue
+            if left_side in association_rules:
+                association_rules[left_side].append({'recommendations': right_side, 'confidence': confidence})
+            else:
+                association_rules.update({left_side: [{'recommendations': right_side, 'confidence': confidence}]})
+
+    pprint(association_rules)
 
 
 if __name__ == '__main__':
